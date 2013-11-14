711c2673d4b5481256b1fca580bcb02229c34ec3
Greg Allen <me@jga.me>
2013-11-14 09:06:14 -0800

    fixed twitter share

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
         analytics: 'UA-24017782-2',
         github: 'github.com/firstandthird/load-grunt-config',
         builtBy: 'First + Third',
-        twitterShare: 'load-grunt-config - Modularize your Gruntfile http://firstandthird.github.io/load-grunt-config/'
+        twitterShare: 'load-grunt-config - Modularize your Gruntfile'
       }
     }
   });

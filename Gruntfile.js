e6f341e136df4cc8349b438c78d365c5655b57cd
Greg Allen <me@jga.me>
2013-11-21 10:46:46 -0800

    initial work on setting up auto gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,17 @@
 module.exports = function(grunt) {
 
-  require('./lib/load-config')(grunt);
+  require('./lib/load-config')(grunt, {
+    config: {
+      doc: 'README.md',
+      design: 'clean',
+      data: {
+        pageTitle: 'load-grunt-config - Modularize your Gruntfile',
+        analytics: 'UA-24017782-2',
+        github: 'github.com/firstandthird/load-grunt-config',
+        builtBy: 'First + Third',
+        twitterShare: 'load-grunt-config - Modularize your Gruntfile'
+      }
+    }
+  });
 
 };

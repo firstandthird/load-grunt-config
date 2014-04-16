04bfbd8a5c05587040ac27bed0b37cb9f3b21ec1
Greg Allen <me@jga.me>
2014-04-16 10:27:17 -0700

    fixed ghpage config

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,16 @@ module.exports = function(grunt) {
 
   require('./index')(grunt, {
     config: {
-      doc: 'README.md',
-      design: 'clean',
       data: {
-        pageTitle: 'load-grunt-config - Modularize your Gruntfile',
-        analytics: 'UA-24017782-2',
-        github: 'github.com/firstandthird/load-grunt-config',
-        builtBy: 'First + Third',
-        twitterShare: 'load-grunt-config - Modularize your Gruntfile'
+        doc: 'README.md',
+        design: 'clean',
+        ghPageData: {
+          pageTitle: 'load-grunt-config - Modularize your Gruntfile',
+          analytics: 'UA-24017782-2',
+          github: 'github.com/firstandthird/load-grunt-config',
+          builtBy: 'First + Third',
+          twitterShare: 'load-grunt-config - Modularize your Gruntfile'
+        }
       }
     }
   });

dc1ed3944d6b2334a4276ffaab0ed0041fd29416
tawez <tawezik@gmail.com>
2014-09-08 09:28:26 +0200

    configPath accepts array of strings

--- test/gruntconfig.test.js
+++ test/gruntconfig.test.js
@@ -7,11 +7,40 @@ var expected = require('./fixtures/output');
 
 suite('gruntConfig', function() {
 
-  test('basic with data', function() {
 
+  test('no configPath', function () {
     var grunt = {};
     var options = {
-      configPath: __dirname+'/config',
+      data: {
+        test: 1
+      }
+    };
+
+    var config = gruntConfig(grunt, options);
+
+    assert.deepEqual(config, {});
+  });
+
+
+  test('empty configPath', function () {
+    var grunt = {};
+    var options = {
+      configPath: [],
+      data: {
+        test: 1
+      }
+    };
+
+    var config = gruntConfig(grunt, options);
+
+    assert.deepEqual(config, {});
+  });
+
+
+  test('single configPath', function() {
+    var grunt = {};
+    var options = {
+      configPath: __dirname + '/config',
       data: {
         test: 1
       }
@@ -21,12 +50,14 @@ suite('gruntConfig', function() {
     assert.deepEqual(config, expected);
   });
 
-  test('single overridePath as string', function() {
 
+  test('multiple configPath', function () {
     var grunt = {};
     var options = {
-      configPath: __dirname+'/config',
-      overridePath: __dirname+'/config/override',
+      configPath: [
+        __dirname + '/config',
+        __dirname + '/config/override'
+      ],
       data: {
         test: 1
       }
@@ -37,15 +68,13 @@ suite('gruntConfig', function() {
     var expectedClone = _.clone(expected, true);
     expectedClone.jsobj.jsobjFile.options.filename = 'override';
     assert.deepEqual(config, expectedClone);
-
   });
 
-  test('single overridePath as Array<string>', function() {
 
+  test('empty overridePath', function () {
     var grunt = {};
     var options = {
-      configPath: __dirname+'/config',
-      overridePath: [__dirname+'/config/override'],
+      overridePath: [],
       data: {
         test: 1
       }
@@ -53,19 +82,107 @@ suite('gruntConfig', function() {
 
     var config = gruntConfig(grunt, options);
 
-    var expectedClone = _.clone(expected, true);
-    expectedClone.jsobj.jsobjFile.options.filename = 'override';
-    assert.deepEqual(config, expectedClone);
-
+    assert.deepEqual(config, {});
   });
 
-  test('multiple overridePath as Array<string>', function () {
+
+  test('single overridePath', function() {
     var grunt = {};
     var options = {
-      configPath: __dirname+'/config',
+      overridePath: __dirname + '/config',
+      data: {
+        test: 1
+      }
+    };
+
+    var config = gruntConfig(grunt, options);
+    assert.deepEqual(config, expected);
+  });
+
+
+  test('multiple overridePath', function () {
+    var grunt = {};
+    var options = {
       overridePath: [
-        __dirname+'/config/override',
-        __dirname+'/config/another-override',
+        __dirname + '/config',
+        __dirname + '/config/override'
+      ],
+      data: {
+        test: 1
+      }
+    };
+
+    var config = gruntConfig(grunt, options);
+
+    var expectedClone = _.clone(expected, true);
+    expectedClone.jsobj.jsobjFile.options.filename = 'override';
+    assert.deepEqual(config, expectedClone);
+  });
+
+
+  test('empty configPath and overridePath', function () {
+    var grunt = {};
+
+    var options = {
+      configPath: [],
+      overridePath: [],
+      data: {
+        test: 1
+      }
+    };
+
+    var config = gruntConfig(grunt, options);
+
+    assert.deepEqual(config, {});
+  });
+
+
+  test('single configPath with single overridePath', function () {
+    var grunt = {};
+    var options = {
+      configPath: __dirname + '/config',
+      overridePath: __dirname + '/config/override',
+      data: {
+        test: 1
+      }
+    };
+
+    var config = gruntConfig(grunt, options);
+
+    var expectedClone = _.clone(expected, true);
+    expectedClone.jsobj.jsobjFile.options.filename = 'override';
+    assert.deepEqual(config, expectedClone);
+  });
+
+
+  test('multiple configPath with single overridePath', function () {
+    var grunt = {};
+    var options = {
+      configPath: [
+        __dirname + '/config',
+        __dirname + '/config/override'
+      ],
+      overridePath: __dirname + '/config/another-override',
+      data: {
+        test: 1
+      }
+    };
+
+    var config = gruntConfig(grunt, options);
+
+    var expectedClone = _.clone(expected, true);
+    expectedClone.jsobj.jsobjFile.options.filename = 'another-override';
+    assert.deepEqual(config, expectedClone);
+  });
+
+
+  test('single configPath with multiple overridePath', function () {
+    var grunt = {};
+    var options = {
+      configPath: __dirname + '/config',
+      overridePath: [
+        __dirname + '/config/override',
+        __dirname + '/config/another-override'
       ],
       data: {
         test: 1
@@ -77,7 +194,30 @@ suite('gruntConfig', function() {
     var expectedClone = _.clone(expected, true);
     expectedClone.jsobj.jsobjFile.options.filename = 'another-override';
     assert.deepEqual(config, expectedClone);
+  });
 
+
+  test('multiple configPath with multiple overridePath', function () {
+    var grunt = {};
+    var options = {
+      configPath: [
+        __dirname + '/config',
+        __dirname + '/config/override'
+      ],
+      overridePath: [
+        __dirname + '/config/another-override',
+        __dirname + '/config/override'
+      ],
+      data: {
+        test: 1
+      }
+    };
+
+    var config = gruntConfig(grunt, options);
+
+    var expectedClone = _.clone(expected, true);
+    expectedClone.jsobj.jsobjFile.options.filename = 'override';
+    assert.deepEqual(config, expectedClone);
   });
 
 });
